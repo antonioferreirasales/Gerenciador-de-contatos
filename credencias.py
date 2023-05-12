2606ef17a85252a6120a4baa3986fbc277fe5bf5
Antônio Sales <antoniosales1003@gmail.com>
2023-05-12 10:58:07 -0300

    Adicionando integração com banco de dados PostgreSQL. Cadastro de cliente e pesquisa.

---
+++ credencias.py
@@ -0,0 +1,10 @@
+# Credencias do banco de dados
+# Substitua por suas credencias, removendo as descrições e os colchetes que as entornam.
+ip_banco = "[IP DO BANCO DE DADOS]"
+porta_banco = "[PORTA DO BANCO DE DADOS]"
+nome_banco = "[NOME DO BANCO DE DADOS]"
+usuario_banco = "[USUÁRIO DO BANCO DE DADOS]"
+senha_banco = "[SENHA DO USUÁRIO]"
+
+
+
--- funcoes.py
+++ funcoes.py
@@ -1,64 +1,103 @@
+from credencias import *
+import psycopg2
+from datetime import datetime
+
 clientes = {}
+hora_execucao = datetime.now()
 
-#função para deixar as mensagens vermelhas
-def deixaVermelho(msg):
-    print('\033[31m'+msg+'\033[m')
+conn = psycopg2.connect(
+    f"host={ip_banco} port={porta_banco} dbname={nome_banco} user={usuario_banco} password={senha_banco}"
+)
+cursor = conn.cursor()
 
 
-#função para ler e tratar os input do menu
-def lerMensagem(msg):
+# função para deixar as mensagens vermelhas
+def deixa_vermelho(msg):
+    print('\033[31m' + msg + '\033[m')
+
+
+# função para ler e tratar os input do menu
+def ler_mensagem(msg):
     while True:
         try:
             n = int(input(msg))
         except(ValueError, TypeError):
-            deixaVermelho('Erro: digite uma opção válida!')
+            deixa_vermelho('Erro: digite uma opção válida!')
             continue
-        except(KeyboardInterrupt):
+        except KeyboardInterrupt:
             return 0
         else:
             return n
 
 
-## função apenas para imprimir linhas
-def linha (tamanho=42):
+# função apenas para imprimir linhas
+def linha(tamanho=42):
     return '-' * tamanho
 
 
-#função de um molde para texto destacados
+# função de um molde para texto destacados
 def molde(txt):
     print(linha())
     print(txt.center(42))
     print(linha())
 
 
-#função dinâmica para criação do menu
+# f unção dinâmica para criação do menu
 def menu(lista):
     molde('Menu')
-    c = 1 
+    c = 1
     for item in lista:
         print(f'{c} - {item}')
-        c+=1
+        c += 1
     print(linha())
-    opcao = lerMensagem('Digite sua opção: ')
+    opcao = ler_mensagem('Digite sua opção: ')
     return opcao
 
-#cadastrar um cliente com nome e telefone
-def cadastraCliente(nome, telefone):
-    clientes[nome] = telefone
+
+# cadastrar um cliente com nome e telefone
+def cadastra_cliente(nome, telefone, data_nascimento, observacao):
+    global telefone_id, telefone_data_atualizacao
+    cursor.execute("INSERT INTO telefone ("
+                   "telefone_numero, telefone_data_atualizacao, tipo_telefone_id"
+                   ") VALUES (%s, %s, 1)", (telefone, hora_execucao))
+
+    cursor.execute(
+        "SELECT telefone_id, telefone_data_atualizacao FROM telefone WHERE telefone_numero = %s", (telefone,))
+    result = cursor.fetchone()
+    if result is not None:
+        telefone_id, telefone_data_atualizacao = result
+    else:
+        deixa_vermelho("Valores são vazios!")
+
+    if observacao == '':
+        observacao = None
+    cursor.execute("INSERT INTO contato ("
+                   "contato_nome, telefone_id, contato_nascimento, contato_observacao, contato_data_criacao, "
+                   "contato_data_atualizacao"
+                   ") VALUES (%s, %s, %s, %s, %s, %s)",
+                   (nome, telefone_id, data_nascimento, observacao, telefone_data_atualizacao, telefone_data_atualizacao,))
+    conn.commit()
     print('Cliente cadastrado com sucesso!')
 
 
-#procurar um cliente pelo nome, mostrando o telefone
-def procuraCliente(nome):
-    telefoneCliente = clientes[nome]
-    print(f'Cliente: {nome}, Telefone: {telefoneCliente}')
+# procurar um cliente pelo nome, mostrando o telefone
+def procura_cliente(nome):
+    cursor.execute(
+        "SELECT c.contato_nome, t.telefone_numero, c.contato_observacao FROM contato c INNER JOIN telefone t ON "
+        "c.telefone_id ="
+        "t.telefone_id WHERE c.contato_nome = %s"
+        , (nome,))
+    result = cursor.fetchone()
+    contato_nome, contato_telefone, contato_observacao = result
+    print(f'Cliente: {contato_nome}, Telefone: {contato_telefone}. || Obs: {contato_observacao}')
 
 
-#altera o telefone do cliente
-def alteraCliente(nome, telefone):
+# altera o telefone do cliente
+def altera_cliente(nome, telefone):
     clientes[nome] = telefone
     print('Cliente alterado com sucesso!')
 
-#deleta um cliente
-def deletaCliente(nome):
+
+# deleta um cliente
+def deleta_cliente(nome):
     clientes.pop(nome)
