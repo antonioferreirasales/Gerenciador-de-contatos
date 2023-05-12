5eff65d9f26100f2ecee86515b098ef64a6a6e40
Antônio Sales <antoniosales1003@gmail.com>
2023-05-12 09:53:03 -0300

    Adicionando integração com banco de dados PostgreSQL. Cadastro de cliente e pesquisa.

---
+++ credencias.py
@@ -0,0 +1,11 @@
+# Credencias do banco de dados
+ip_banco = "192.168.1.172"
+porta_banco = "5432"
+nome_banco = "gerenciador_contatos"
+usuario_banco = "postgres"
+senha_banco = "antonios1"
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-from funcoes import*
+from funcoes import *
 from time import sleep
+
+conn = psycopg2.connect(
+    f"host={ip_banco} port={porta_banco} dbname={nome_banco} user={usuario_banco} password={senha_banco}"
+)
+
 while True:
     resposta = menu(['Cadastrar um cliente', 'Pesquisar um cliente',
-                    'Alterar um cliente', 'Remover um cliente', 'Sair'])
+                     'Alterar um cliente', 'Remover um cliente', 'Sair'])
 
     if resposta == 1:
         nome = input('Digite o nome do cliente: ')
-        telefone = int(input('Digite o número do cliente: '))
-        cadastraCliente(nome, telefone)
-
+        telefone = input('Digite o número do cliente: ')
+        data_nascimento = input('Qual a data de nascimento do cliente: ')
+        pergunta_observacao = input('Gostaria de adicionar uma observação? s / n \n')
+        if pergunta_observacao == 's':
+            observacao = input('Qual observação gostaria de adicionar? \nDigite: ')
+        else:
+            observacao = None
+        cadastra_cliente(nome, telefone, data_nascimento, observacao)
     elif resposta == 2:
         nome = input('Digite o nome do cliente: ')
-        procuraCliente(nome)
+        procura_cliente(nome)
 
     elif resposta == 3:
         nome = input('Digite o nome do cliente que você quer alterar: ')
         telefone = input('Digite o novo número de telefone: ')
-        alteraCliente(nome, telefone)
+        altera_cliente(nome, telefone)
 
     elif resposta == 4:
         nome = input('Digite o nome do cliente que você quer remover: ')
-        deletaCliente(nome)
+        deleta_cliente(nome)
         print('Cliente removido com sucesso!')
 
     elif resposta == 5:
@@ -28,5 +38,7 @@ while True:
         break
 
     else:
-        deixaVermelho('Erro: digite uma opção válida!')
+        deixa_vermelho('Erro: digite uma opção válida!')
     sleep(1)
+cursor.close()
+conn.close()
